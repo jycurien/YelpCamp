a5adcfb2be302094eb2e7191978fc8810f22d2e9
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-13 18:43:30 +0100

    seeding campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -18,15 +17,6 @@ app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
-})
-
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({
-    title: 'My Backyard',
-    description: 'Cheap camping',
-  })
-  await camp.save()
-  res.send(camp)
 })
 
 app.listen(3000, () => console.log('LISTENING ON PORT 3000'))
