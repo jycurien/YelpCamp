063a4f57c993b102b729d947ac9d503598503436
Jean-Yves Curien <jycurien@ime.local>
2022-01-14 17:20:51 +0100

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eaque laudantium nulla perspiciatis quibusdam a, magni enim odit amet id officia sed veritatis blanditiis! Nam debitis ipsa eaque quibusdam dolorem.',
+      price,
     })
     await camp.save()
   }
