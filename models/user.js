86958376e510b86197df6fdc3390a4a52d89fb4b
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-19 18:48:14 +0100

    configuring passport

--- models/user.js
+++ models/user.js
@@ -9,6 +9,6 @@ const UserSchema = new Schema({
     unique: true,
   },
 })
-userSchema.plugin(passportLocalMongoose)
+UserSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', UserSchema)
