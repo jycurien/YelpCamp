a2b657f5c6a9aae933c923c84630796d4f8e14f8
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-20 18:37:40 +0100

    add author to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,9 +31,9 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.get(
   '/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      'reviews'
-    )
+    const campground = await Campground.findById(req.params.id)
+      .populate('reviews')
+      .populate('author')
     if (!campground) {
       req.flash('error', 'Cannot find that campground')
       return res.redirect('/campgrounds')
@@ -48,6 +48,7 @@ router.post(
   validateCampground,
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
+    campground.author = req.user._id
     await campground.save()
     req.flash('success', 'Successfuly made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
