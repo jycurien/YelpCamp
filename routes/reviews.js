c2fc27b78d12c33244e09013ad799ab2b53046e9
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-20 19:48:23 +0100

    review permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true }) // mergeParams to have access to all the request params
 const catchAsync = require('../utils/catchAsync')
-const { validateReview } = require('../middleware')
+const { validateReview, isLoggedIn } = require('../middleware')
 const Campground = require('../models/campground')
 const Review = require('../models/review')
 
 router.post(
   '/',
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id)
     const review = new Review(req.body.review)
+    review.author = req.user._id
     campground.reviews.push(review)
     await review.save()
     await campground.save()
@@ -21,6 +23,7 @@ router.post(
 
 router.delete(
   '/:reviewId',
+  isLoggedIn,
   catchAsync(async (req, res, next) => {
     const { id, reviewId } = req.params
     await Campground.findByIdAndUpdate(id, {
