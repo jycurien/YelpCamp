375240fd237058ee05c03cfc4abab33029c8799f
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-21 22:50:51 +0100

    saving mapbox geometry

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const Campground = require('../models/campground')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapBoxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({})
@@ -11,10 +14,17 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send()
   const campground = new Campground({
     ...req.body.campground,
     images: req.files.map((f) => f.path.slice(6)),
   })
+  campground.geometry = geoData.body.features[0].geometry
   campground.author = req.user._id
   await campground.save()
   req.flash('success', 'Successfuly made a new campground!')
