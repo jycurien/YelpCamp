6063cfe0dbd520e976ef300df018dc53a09e3651
Jean-Yves Curien <jy.curien@laposte.net>
2022-01-23 13:00:12 +0100

    mongostore for sessions

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize')
+// const helmet = require('helmet')
+const MongoStore = require('connect-mongo')
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
@@ -37,7 +40,64 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
+// app.use(helmet())
+// const scriptSrcUrls = [
+//   'https://stackpath.bootstrapcdn.com',
+//   'https://api.tiles.mapbox.com',
+//   'https://api.mapbox.com',
+//   'https://kit.fontawesome.com',
+//   'https://cdnjs.cloudflare.com',
+//   'https://cdn.jsdelivr.net',
+// ]
+// const styleSrcUrls = [
+//   'https://kit-free.fontawesome.com',
+//   'https://stackpath.bootstrapcdn.com',
+//   'https://api.mapbox.com',
+//   'https://api.tiles.mapbox.com',
+//   'https://fonts.googleapis.com',
+//   'https://use.fontawesome.com',
+//   'https://cdn.jsdelivr.net',
+// ]
+// const connectSrcUrls = [
+//   'https://api.mapbox.com',
+//   'https://*.tiles.mapbox.com',
+//   'https://events.mapbox.com',
+// ]
+// const fontSrcUrls = []
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: [],
+//       connectSrc: ["'self'", ...connectSrcUrls],
+//       scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//       workerSrc: ["'self'", 'blob:'],
+//       childSrc: ['blob:'],
+//       objectSrc: [],
+//       imgSrc: [
+//         "'self'",
+//         'blob:',
+//         'data:',
+//         'https://via.placeholder.com',
+//         'https://images.unsplash.com',
+//       ],
+//       fontSrc: ["'self'", ...fontSrcUrls],
+//     },
+//   })
+// )
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  secret: 'thisshouldbeabettersecret',
+  touchAfter: 24 * 60 * 60,
+})
+
+store.on('error', function (e) {
+  console.log('SESSION STORE ERROR', e)
+})
+
 const sessionConfig = {
+  store,
   name: 'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
